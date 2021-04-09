df95e31e52269510597605b65f0e6061944eef64
Patricio Rodríguez - DaVinci <patricio.rodriguez@davinci.edu.ar>
2021-04-09 20:10:50 -0300

    v0.2
    Los objetos importantes (Tag == "KeyObject") reconocen cuándo se les hace click cierta distancia.
    Se agregó una pequeña mira negra.

---
+++ Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+    public Transform  cam;
+           RaycastHit hit;
+    public float      rayLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rayLength = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.Raycast(cam.position, cam.forward, out hit, 0f);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.DrawRay(cam.position, cam.forward * rayLength, Color.red);
+
+            if(Physics.Raycast(cam.position, cam.forward, out hit, rayLength))
+            {
+                if (hit.collider.tag == "KeyObject")
+
+                    Debug.Log("Activar objeto");
+
+                if (Input.GetKeyDown(KeyCode.E))
+
+                    Debug.Log("Se está usando el objeto");
+            }
+        }
+    }
+}
